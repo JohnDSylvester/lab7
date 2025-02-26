3427ed7956120ed04e8ca3edf33ada22c3739f8a
John Sylvester <johnsylvester@csilvm-13.cs.ucsb.edu>
2025-02-25 21:35:31 -0800

    next negative attempt

--- labs/typo/Heap.cpp
+++ labs/typo/Heap.cpp
@@ -104,20 +104,13 @@ void fixPop(Heap::Entry* data, size_t index, size_t count){
 	size_t right = index* 2 + 2;
 	size_t current = index;
 
-	if(right < count && data[right].score< data[current].score){
-		current = right;
-	}
-	
 	if(left < count && data[left].score < data[current].score){
                 current = left;
         }
-	if(right < count && left < count && data[left].score < data[current].score && data[right].score == data[left].score){
-		current = left;
-	}
 
-	//if(left < count && right < count && data[left].score == data[right].score){
-          //      current = left;
-        //}
+	if(right < count && data[right].score< data[current].score){
+		current = right;
+	}
 
 	if(current != index){
 		Heap::Entry temp = data[index];
@@ -144,7 +137,7 @@ bool topCheck(Heap::Entry* data, size_t &index){
 
 bool bottomCheck(Heap::Entry* data, size_t &index, size_t count){
         if(index* 2 + 2 < count){
-	bool leftLowest = data[index].score > data[(index * 2) + 1].score && data[(index*2)+2].score > data[(index * 2) + 1].score;
+	bool leftLowest = data[index].score > data[(index * 2) + 1].score && data[(index*2)+2].score >= data[(index * 2) + 1].score;
 	bool rightLowest = data[index].score > data[(index * 2) + 2].score && data[(index*2)+1].score > data[(index * 2) + 2].score;
 	if(leftLowest){
 		Heap::Entry temp = data[index];
@@ -159,13 +152,6 @@ bool bottomCheck(Heap::Entry* data, size_t &index, size_t count){
                 data[index * 2 + 2] = temp;
                 index = index * 2 + 2;
 		return 1;
-	}
-	else if(data[(index * 2) + 1].score == data[(index*2)+2].score && data[(index * 2) + 1].score != data[index].score && data[(index * 2) + 1].score < data[index].score){
-		Heap::Entry temp = data[index];
-                data[index] = data[index * 2 + 1];
-                data[index * 2 + 1] = temp;
-                index = index * 2 + 1;
-                return 0;
 	}
 	}
 	else if(index* 2 + 1 < count){
